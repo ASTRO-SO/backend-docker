d1c9fa707bb2f68189282078c48e12f47e6cbea6
Golden Diva <trandoanductai@gmail.com>
2025-06-16 13:12:11 +0700

    Update app.js

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(cors({
     'http://localhost:3000',  // React default
     'http://localhost',       // Your current frontend
     'http://localhost:8080'   // Common dev server
+    ,'https://goldeneden.io.vn'
   ],
   credentials: true
 }));
